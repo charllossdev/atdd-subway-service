daf1375a4810e4a422bc59d042073db26c48d943
charlloss.dev <charlloss.dev@gmail.com>
2021-01-19 23:51:22 +0900

    refactor(fare): 메서드 복잡성 정리 피드백 반영

--- src/main/java/nextstep/subway/path/domain/fare/FareBuilder.java
+++ src/main/java/nextstep/subway/path/domain/fare/FareBuilder.java
@@ -3,7 +3,6 @@ package nextstep.subway.path.domain.fare;
 import java.util.List;
 
 import nextstep.subway.auth.domain.LoginMember;
-import nextstep.subway.line.domain.Distance;
 import nextstep.subway.line.domain.Lines;
 import nextstep.subway.path.domain.PathFinder;
 import nextstep.subway.station.domain.Station;
@@ -12,16 +11,12 @@ public class FareBuilder {
 
 	public static Money calculate(LoginMember loginMember, PathFinder finder) {
 		FareUserAge ageFare = FareUserAge.findFareAge(loginMember.getAge());
-		Money distanceFare = calculateDistanceFare(finder.distance(), ageFare);
+		Money distanceFare = FareDistance.calculateDistance(finder.distance(), ageFare);
 		Money lineAddFare = calculateLineAddFare(finder.getLines(), finder.stations(), ageFare);
 
 		return distanceFare.plus(lineAddFare);
 	}
-
-	private static Money calculateDistanceFare(Distance distance, FareUserAge ageFare) {
-		return FareDistance.calculateDistance(distance, ageFare);
-	}
-
+	
 	private static Money calculateLineAddFare(Lines lines, List<Station> stations, FareUserAge ageFare) {
 		return ageFare.discount(lines.maximumLineFare(stations));
 	}
