e982b65ad1b9d390f17eafc0ed101cf6301540a5
charlloss.dev <charlloss.dev@gmail.com>
2021-01-19 23:51:49 +0900

    refactor(distance): 코드 반복 정리 피드백 반영

--- src/main/java/nextstep/subway/path/domain/fare/FareDistance.java
+++ src/main/java/nextstep/subway/path/domain/fare/FareDistance.java
@@ -1,32 +1,26 @@
 package nextstep.subway.path.domain.fare;
 
 import java.util.Arrays;
-import java.util.function.BiFunction;
 
 import lombok.AllArgsConstructor;
 import nextstep.subway.line.domain.Distance;
 import nextstep.subway.path.domain.fare.distance.DefaultDistanceFare;
+import nextstep.subway.path.domain.fare.distance.DistanceFare;
 import nextstep.subway.path.domain.fare.distance.LongDistanceFare;
 import nextstep.subway.path.domain.fare.distance.MiddleDistanceFare;
 
 @AllArgsConstructor
 public enum FareDistance {
-	SHORT_DISTANCE(0, 10, (distance, ageFare) -> {
-		return new DefaultDistanceFare().calculate(distance, ageFare);
-	}),
-	MIDDLE_DISTANCE(11, 50, (distance, ageFare) -> {
-		return new MiddleDistanceFare().calculate(distance, ageFare);
-	}),
-	LONG_DISTANCE(51, Integer.MAX_VALUE, (distance, ageFare) -> {
-		return new LongDistanceFare().calculate(distance, ageFare);
-	});
+	SHORT_DISTANCE(0, 10, new DefaultDistanceFare()),
+	MIDDLE_DISTANCE(11, 50, new MiddleDistanceFare()),
+	LONG_DISTANCE(51, Integer.MAX_VALUE, new LongDistanceFare());
 
 	private final int minimumDistance;
 	private final int maximumDistance;
-	private final BiFunction<Distance, FareUserAge, Money> expression;
+	private final DistanceFare distanceFare;
 
 	public static Money calculateDistance(final Distance distance, final FareUserAge ageFare) {
-		return findDistanceFareType(distance.getDistance()).expression.apply(distance, ageFare);
+		return findDistanceFareType(distance.getDistance()).calculate(distance, ageFare);
 	}
 
 	private static FareDistance findDistanceFareType(final int distance) {
@@ -34,5 +28,9 @@ public enum FareDistance {
 			.filter(it -> distance >= it.minimumDistance && distance <= it.maximumDistance)
 			.findFirst()
 			.orElseThrow(IllegalArgumentException::new);
+	}
+
+	private Money calculate(Distance distance, FareUserAge ageFare) {
+		return this.distanceFare.calculate(distance, ageFare);
 	}
 }
